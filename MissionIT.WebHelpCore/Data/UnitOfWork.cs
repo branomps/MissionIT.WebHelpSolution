2c1edb281397ccbd94227c59be8923aa3082e5e3
BranislavMacosinec <branomps@gmail.com>
2018-07-15 21:08:16 +0100

    Add UnitOfWork.

---
+++ MissionIT.WebHelpCore/Data/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using MissionIT.WebHelpCore.Models.ModelRepository;
+using MissionIT.WebHelpCore.Models.HelpClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionIT.WebHelpCore.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly HelpDbContext _context;
+
+        public UnitOfWork(HelpDbContext context)
+        {
+            _context = context;
+            Sections = new SectionRepository(_context);
+            Articles = new ArticleRepository(_context);
+        }
+
+        public ISectionRepository Sections { get; set; }
+        public IArticleRepository Articles { get; set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
