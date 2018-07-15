47756190ce97b09509260f00952928e81c31aa56
BranislavMacosinec <branomps@gmail.com>
2018-07-15 20:18:58 +0100

    Add project files.

---
+++ MissionIT.WebHelpCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MissionIT.WebHelpCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
