39cdf53ec3df733d7fe5c87850267888d4fbc1f9
BranislavMacosinec <branomps@gmail.com>
2018-07-15 21:12:51 +0100

    Add SectionRepository.

---
+++ MissionIT.WebHelpCore/Data/SectionRepository.cs
@@ -0,0 +1,30 @@
+﻿using MissionIT.WebHelpCore.Models.HelpClasses;
+using MissionIT.WebHelpCore.Models.ModelRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionIT.WebHelpCore.Data
+{
+    public class SectionRepository : Repository<Section>, ISectionRepository
+    {
+        public SectionRepository(HelpDbContext context)
+            : base(context)
+        {
+
+        }
+
+        public HelpDbContext HelpDbContext
+        {
+            get { return Context as HelpDbContext; }
+            // set { HelpDbContext = value; }
+        }
+
+        Section ISectionRepository.GetSectionWithArticles(int id)
+        {
+            return HelpDbContext.Sections.Include(a => a.SectionID == id).SingleOrDefault(a => a.SectionID == id);
+        }
+    }
+}
