f34e4648fbf32d996b2e7bf524cc9fb59a7e9a2f
BranislavMacosinec <branomps@gmail.com>
2018-07-15 20:34:39 +0100

    Add Interface IUnitOfWork to ModelRepository.

---
+++ MissionIT.WebHelpCore/Models/ModelRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionIT.WebHelpCore.Models.ModelRepository
+{
+    interface IUnitOfWork : IDisposable
+    {
+        ISectionRepository Sections { get; }
+        IArticleRepository Articles { get; }
+        int Complete();
+    }
+}
