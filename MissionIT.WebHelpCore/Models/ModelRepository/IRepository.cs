5f4c81c4f3035cd50e58b355db72a5447cde5a36
BranislavMacosinec <branomps@gmail.com>
2018-07-15 20:37:26 +0100

    Add Interface Generic IRepository.

---
+++ MissionIT.WebHelpCore/Models/ModelRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MissionIT.WebHelpCore.Models.ModelRepository
+{
+    interface IRepository<T> where T : class 
+    {
+        T GetOne(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        void Add(T tEntity);
+        void Update(T tEntity);
+        void Remove(T tEntity);
+    }
+}
