465aae669e0a71f8381a1683f0c25532ae420ca6
BranislavMacosinec <branomps@gmail.com>
2018-07-15 21:15:10 +0100

    Add ArticleRepository.

---
+++ MissionIT.WebHelpCore/Data/ArticleRepository.cs
@@ -0,0 +1,29 @@
+﻿using MissionIT.WebHelpCore.Models.HelpClasses;
+using MissionIT.WebHelpCore.Models.ModelRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionIT.WebHelpCore.Data
+{
+    public class ArticleRepository : Repository<Article>, IArticleRepository
+    {
+        public ArticleRepository(HelpDbContext context)
+            : base(context)
+        {
+
+        }
+
+        public HelpDbContext HelpDbContext
+        {
+            get { return Context as HelpDbContext; }
+            //set { HelpDbContext = value; }
+        }
+
+        public IEnumerable<Article> GetArticleWithSection()
+        {
+            return HelpDbContext.Articles.ToList();
+        }
+    }
+}
